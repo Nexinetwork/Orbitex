e3ed4b00f69883b04901fb39ae6d48471070e4cd
git <eae966@gmail.com>
2024-01-11 10:30:44 +0330

    Orbitex image has been updated.

--- nexi-swap-interface-v1/src/components/CurrencyLogo/index.tsx
+++ nexi-swap-interface-v1/src/components/CurrencyLogo/index.tsx
@@ -46,7 +46,7 @@ export default function CurrencyLogo({
   }, [currency, uriLocations])
 
   if (currency === ETHER) {
-    return <StyledBnbLogo src="/images/coins/0xfB000De07a23Bd3670e94e5F02A5a293424B98B6.png" size={size} style={style} />
+    return <StyledBnbLogo src="/images/coins/0x74cA89e0a98145feA0142ECf9fB7b6964585621E.png" size={size} style={style} />
   }
 
   return (currency as any)?.symbol ? (
